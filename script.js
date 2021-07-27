b837ef7ef6656db01506356bd00243e4b3a7ff7b
Andre Le <andrelezx@gmail.com>
2021-07-27 08:59:05 -0600

    Added computerPlay function to script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function computerPlay() {
+    let choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0: return "Rock";
+        case 1: return "Paper";
+        default: return "Scissors";
+    }
+}
