04b92496d90739eac6ccea2737e9401103501033
Andre Le <andrelezx@gmail.com>
2021-07-27 09:10:31 -0600

    Added playRound function to script.js

--- script.js
+++ script.js
@@ -6,3 +6,35 @@ function computerPlay() {
         default: return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection.trim();
+    playerSelection = playerSelection[0].toUpperCase() +
+            playerSelection.slice(1).toLowerCase();
+
+    let result;
+    if (playerSelection === computerSelection) {return "It's a tie";}
+    else if (playerSelection === "Rock") {
+        result = computerSelection === "Scissors" ?
+                "You win!" : "You lose!";
+    }
+    else if (playerSelection === "Paper") {
+        result = computerSelection === "Rock" ?
+                "You win!" : "You lose!";
+    }
+    else if (playerSelection === "Scissors") {
+        result = computerSelection === "Paper" ?
+                "You win!" : "you lose!";
+    }
+    else {return "Invalid";}
+
+    if (result.includes("win")) {
+        return `${result} ${playerSelection} beats ${computerSelection}`;
+    }
+    else {
+        return `${result} ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerSelection = prompt("Make your move");
+const computerSelection = computerPlay();
